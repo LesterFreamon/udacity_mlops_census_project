0b35ad3bca727d2f61acf218229a3f5fea3e6bae
Adam Arnon <adamarnon@gmail.com>
2023-06-21 16:27:16 -0700

    start simple

---
+++ starter/starter/clean_data.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+def clean_column_names(df: pd.DataFrame) -> pd.DataFrame:
+    """Cleans the column names"""
+    df.columns = df.columns.str.replace(' ', '')  # Delete spaces
+    df.columns = df.columns.str.lower()  # Convert to lowercase
+    df.columns = df.columns.str.replace('-', '_')  # Replace hyphen with underscore
+    return df
+
+def clean_string_columns(df: pd.DataFrame) -> pd.DataFrame:
+    """Clean the object columns"""
+    # Select columns where dtype is object
+    string_columns = df.select_dtypes(include=['object']).columns
+
+    # Apply transformations to each string column
+    for col in string_columns:
+        df[col] = df[col].str.replace(' ', '')  # Delete spaces
+        df[col] = df[col].str.lower()  # Convert to lowercase
+        df[col] = df[col].str.replace('-', '_')  # Replace hyphen with underscore
+
+        # Convert to categorical
+        df[col] = df[col].astype('category')
+
+    return df
+
+def clean_data(raw_df: pd.DataFrame) -> pd.DataFrame:
+    """Cleans orginal file model input data."""
+    clean_col_df = clean_column_names(raw_df)
+    clean_df = clean_string_columns(clean_col_df)
+    return clean_df
---
+++ starter/tests/test_clean_data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from starter.clean_data import clean_column_names, clean_string_columns, clean_data
+
+
+def test_clean_column_names():
+    raw_df = pd.DataFrame({
+        ' Column Name ': np.arange(10),
+        'Column-Name': np.arange(10, 20)
+    })
+    cleaned_df = clean_column_names(raw_df)
+    assert 'columnname' in cleaned_df.columns
+    assert 'column_name' in cleaned_df.columns
+
+
+def test_clean_string_columns():
+    raw_df = pd.DataFrame({
+        'column1': [' Value ', 'Value-', 'VALUE ', 'vAlUe-'] * 5,
+        'column2': np.arange(20)
+    })
+    cleaned_df = clean_string_columns(raw_df)
+    assert cleaned_df['column1'].dtype.name == 'category'
+    assert cleaned_df['column1'].str.contains(' ').any() == False
+    assert cleaned_df['column1'].str.contains('-').any() == False
+    assert cleaned_df['column1'].str.islower().all() == True
+
+
+def test_clean_data():
+    raw_df = pd.DataFrame({
+        ' Column Name ': [' Value ', 'Value-', 'VALUE ', 'vAlUe-'] * 5,
+        'Column-Name': np.arange(20)
+    })
+    cleaned_df = clean_data(raw_df)
+    assert 'columnname' in cleaned_df.columns
+    assert cleaned_df['column_name'].dtype.name == 'category'
+    assert cleaned_df['column_name'].str.contains(' ').any() == False
+    assert cleaned_df['column_name'].str.contains('-').any() == False
+    assert cleaned_df['column_name'].str.islower().all() == True
