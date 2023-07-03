d71d0016839a2d0650b38466411adf3dc87434dd
Adam Arnon <adamarnon@gmail.com>
2023-07-03 10:43:14 -0700

    working end to end

---
+++ starter/src/config.py
@@ -0,0 +1,12 @@
+CAT_FEATURES = [
+    "workclass",
+    "education",
+    "marital_status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native_country",
+]
+
+LABEL = 'salary'
---
+++ starter/tests/test_model.py
@@ -0,0 +1,16 @@
+import pytest
+import numpy as np
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+
+from src.ml.model import compute_model_metrics
+
+
+def test_compute_model_metrics():
+    y = np.array([0, 1, 1, 0, 1, 0, 1, 1, 0, 1])
+    preds = np.array([0, 0, 1, 1, 1, 0, 0, 1, 0, 1])
+
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+
+    assert precision == precision_score(y, preds, zero_division=1)
+    assert recall == recall_score(y, preds, zero_division=1)
+    assert fbeta == fbeta_score(y, preds, beta=1, zero_division=1)
